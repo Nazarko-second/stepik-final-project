7986e0a61af27936f472cb61ef95eb829b378cbf
nazarko-second <nazarko.second@gmail.com>
2019-06-24 18:26:12 +0300

    login_page.py added

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,13 +1,14 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
+from .locators import MainPageLocators
 
 
 class MainPage(BasePage):
 	def go_to_login_page(self):
-		login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-		login_link.click()
+		assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+		#login_link.click()
 
 	def should_be_login_link(self):
-		self.browser.find_element(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
+		assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
 
 #print('__file__={0:<35} | __name__={1:<20} | __package__={2:<20}'.format(__file__,__name__,str(__package__)))
